db5422adbbc07862a6282b38ba439c6285ba59a5
ellenwei <ellenwei@PersonalMicrosoftSoftware.ucla.edu>
2025-02-06 08:32:13 -0500

    remove Hello note

--- src/HelloEllen.js
+++ src/HelloEllen.js
@@ -12,9 +12,6 @@ const HelloEllen = () => {
         <h1>Hello, my name is Ellen</h1>
         <p>I'm a data scientist based in New York City.</p>
         <ScrollButton /> {/* Place the ScrollButton under the text */}
-        <div className="note">
-          <p>*Note: This website is still a work in progress! Web dev is not my specialty...</p>
-        </div>
         
       </div>
     </div>
@@ -22,3 +19,7 @@ const HelloEllen = () => {
 };
 
 export default HelloEllen;
+
+<div className="note">
+<p>*Note: This website is still a work in progress! Web dev is not my specialty...</p>
+</div>
